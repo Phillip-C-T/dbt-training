1c1ce97374636da2df14df9d28f72ae8989d8864
Phill T <Phillipctran@onmail.com>
2022-07-01 05:00:36 +0000

    developed reports_profit_by_customer and reports_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -5,10 +5,11 @@ o.orderid, o.orderdate, o.shipdate, o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordercostprice, o.ordersellingprice,
 -- from raw customer
+c.customerid,
 c.customername, c.segment, c.country,
 
 -- from raw product 
-p.category, p.productname, p.subcategory
+p.productid, p.category, p.productname, p.subcategory
 from {{ ref('raw_orders') }}  as o
 left join {{ ref('raw_customer') }} as c 
 on o.customerid = c.customerid 
