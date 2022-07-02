f8caba66fd6ff710226cdae65e3f6ea5cf645faf
Phill T <Phillipctran@onmail.com>
2022-07-02 05:02:40 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -9,10 +9,14 @@ c.customerid,
 c.customername, c.segment, c.country,
 
 -- from raw product 
-p.productid, p.category, p.productname, p.subcategory
+p.productid, p.category, p.productname, p.subcategory,
+{{ markup( 'ordersellingprice','ordercostprice'  )}} as markup
+
 from {{ ref('raw_orders') }}  as o
 left join {{ ref('raw_customer') }} as c 
 on o.customerid = c.customerid 
 left join {{ ref('raw_product') }} as p 
 on o.productid = p.productid 
+
+/* {{limit_data_in_dev('orderdate')}} */
 
